c48899375b86d69bd3bc147c1ef31b7e5b80aae6
JrFelix540 <juniorfelix8825@outlook.com>
2022-05-31 14:41:04 +0300

    Changes input name

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -4,15 +4,15 @@ import Button from "../Button/Button";
 import { Input } from "../Input/Input";
 
 export const Form: React.FC = () => {
-  const [value, setValue] = useState("");
+  const [email, setEmail] = useState("");
   const [status, setStatus] = useState<string | undefined>(undefined);
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value);
+    setEmail(e.target.value);
   };
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    const response = await sendNetlifyFormRequest("contact", { email: value });
+    const response = await sendNetlifyFormRequest("contact", { email: email });
     console.log(response);
     if (response.ok) {
       setStatus("success");
@@ -22,7 +22,7 @@ export const Form: React.FC = () => {
   };
   return (
     <form name="test" method="POST" data-netlify="true" onSubmit={handleSubmit}>
-      <Input name="test-form" onChange={handleChange} value={value} />
+      <Input name="email" onChange={handleChange} value={email} />
       {status && <p>{status}</p>}
       <Button type="submit">Submit</Button>
     </form>
